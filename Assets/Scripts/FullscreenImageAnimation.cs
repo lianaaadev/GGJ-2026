47ddb44e41d41ee1c8ff407db7e72d1ceeb7cd14
Liana Kan <liana.kan98@gmail.com>
2026-02-01 13:22:35 -0800

    intro comic and music

---
+++ Assets/Scripts/FullscreenImageAnimation.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FullscreenImageAnimation : MonoBehaviour
+{
+    public Image image;
+    public Sprite[] frames;
+    public float frameDuration = 0.1f;
+    public bool isIntro;
+    public GameObject startButton; // drag your button here
+    public AudioSource audio;
+
+    void Start()
+    {
+      if (startButton != null)
+          startButton.SetActive(false);
+      if (audio != null)
+          audio.Play();
+      
+      StartCoroutine(Play());
+    }
+
+    IEnumerator Play()
+    {
+        foreach (Sprite frame in frames)
+        {
+            image.sprite = frame;
+            yield return new WaitForSeconds(frameDuration);
+        }
+
+        if (audio != null)
+            audio.Stop();
+        if (startButton != null)
+            startButton.SetActive(true);
+    }
+}
